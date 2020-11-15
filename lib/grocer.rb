563ea3b79bde6261a78a5a898fe8c3fb2124956c
Andres Hernandez <andres.hernandez@utexas.edu>
2020-11-15 15:03:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -9,28 +9,15 @@ def find_item_by_name_in_collection(name, collection)
   end
   nil
 end
-
 def consolidate_cart(cart)
   # Consult README for inputs and outputs
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
-
-
-
-#create draft consolidated cart
-
+#create cart with unique items
   unique_cart = cart.uniq
-
 #iterate over each item in the cart, counting each unique item
-
-  cart.count do | |
+  unique_cart.map do |unique_item|
+    unique_item[:count] = cart.count {|cart_item| cart_item == unique_item}
   end
-
-  find_item_by_name_in_collection(name, cart)
-
-#with count for each unique item, create new array with only unique items, and append each hash to include count
-
-  cart[:count] = #count of unique item
-
 end
